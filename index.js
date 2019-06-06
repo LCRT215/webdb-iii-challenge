1f94ccdacf03c99173adda054fff7e79b0d2385a
Leslie Thompson <lcrthompson@gmail.com>
2019-06-06 12:37:35 -0700

    half of stretch completed

--- index.js
+++ index.js
@@ -91,6 +91,76 @@ server.delete("/api/cohorts/:id", async (req, res) => {
   } catch (error) {}
 });
 
+/////////////////STUDENT ENDPOINTS////////////////
+
+server.get("/students", async (req, res) => {
+  try {
+    const students = await db("students");
+    res.status(200).json(students);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+server.post("/students", async (req, res) => {
+  try {
+    const [id] = await db("students").insert(req.body);
+
+    const student = await db("students")
+      .where({ id })
+      .first();
+
+    res.status(201).json(student);
+  } catch (error) {
+    const message =
+      errors[error.errno] || "We ran into an error adding the student";
+    res.status(500).json({ message, error });
+  }
+});
+
+server.get("/students/:id", async (req, res) => {
+  // get the roles from the database
+  try {
+    const student = await db("students")
+      .where({ id: req.params.id })
+      .first();
+    res.status(200).json(student);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+server.put("/students/:id", async (req, res) => {
+  try {
+    const count = await db("students")
+      .where({ id: req.params.id })
+      .update(req.body);
+
+    if (count > 0) {
+      const student = await db("students")
+        .where({ id: req.params.id })
+        .first();
+
+      res.status(200).json(student);
+    } else {
+      res.status(404).json({ message: "Student not found" });
+    }
+  } catch (error) {}
+});
+
+server.delete("/students/:id", async (req, res) => {
+  try {
+    const count = await db("students")
+      .where({ id: req.params.id })
+      .del();
+
+    if (count > 0) {
+      res.status(204).end();
+    } else {
+      res.status(404).json({ message: "Student not found" });
+    }
+  } catch (error) {}
+});
 const port = process.env.PORT || 4000;
 server.listen(port, () =>
   console.log(`\n** API running on http://localhost:${port} **\n`)
