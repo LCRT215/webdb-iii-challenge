d09eb6e5ce16346de62f40c94020ac1ef55b8a04
Leslie Thompson <lcrthompson@gmail.com>
2019-06-05 23:41:16 -0700

    debug issues with server not runningproperly. seeds added. get and post added.

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
 const express = require("express");
 const helmet = require("helmet");
 const knex = require("knex");
-// const knexConfig = require("./knexfile");
+const knexConfig = require("./knexfile");
 
-// const db = knex(knexConfig.development);
+const db = knex(knexConfig.development);
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
+
+server.get("/api/cohorts", async (req, res) => {
+  try {
+    const cohorts = await db("cohorts");
+    res.status(200).json(cohorts);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+server.post("/api/cohorts", async (req, res) => {
+  try {
+    const [id] = await db("cohorts").insert(req.body);
+
+    const cohort = await db("cohorts")
+      .where({ id })
+      .first();
+
+    res.status(201).json(cohort);
+  } catch (error) {
+    const message = errors[error.errno] || "We ran into an error";
+    res.status(500).json({ message, error });
+  }
+});
 
 const port = process.env.PORT || 4000;
 server.listen(port, () =>
--- knexfile.js
+++ knexfile.js
@@ -6,43 +6,12 @@ module.exports = {
     connection: {
       filename: "./data/lambda.db3"
     },
+    useNullAsDefault: true,
     migrations: {
       directory: "./data/migrations"
     },
     seeds: {
       directory: "./data/seeds"
-    }
-  },
-
-  staging: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: "knex_migrations"
-    }
-  },
-
-  production: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: "knex_migrations"
     }
   }
 };
